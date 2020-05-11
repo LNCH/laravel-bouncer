e90abab880a4001407d9c609f33d57b766107b94
Tom Lynch <tom@lnch.co.uk>
2020-05-11 21:17:05 +0100

    First commit

---
+++ src/LaravelBouncerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lnch\LaravelBouncer;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBouncerServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
